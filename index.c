233074d934fb8d6267842f0c8fc8000b365584ee
MarceloPrgJr <josemarceloisidorio@gmail.com>
2020-06-20 21:30:10 -0300

    Descrição do programa

---
+++ index.c
@@ -0,0 +1,16 @@
+/*
+*   Programa tem o objetivo de ler cinco números inteiros, ordenar os valores
+* dentro de um array e exibir cada posição ordenada em ordem crescente.
+*
+*   @Autor: Niiick013
+*   @GitHub: https://github.com/Niiick013
+*
+*/
+#include <stdio.h>
+
+int main() {
+    
+
+    return 0;
+
+} //Fim da função main
